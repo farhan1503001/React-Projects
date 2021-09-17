0e076fd0f8d0cc1fe8be946c2267ce877901093a
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-09-17 20:24:27 +0600

    changing navbar

--- restaurant/src/Component/Body/body.js
+++ restaurant/src/Component/Body/body.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Menu from "./Menu";
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+import {Route} from 'react-router-dom'
 class Body extends Component{
     render(){
         return(
             <div>
-                <Menu />
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/menu' exact component={Menu}></Route>
+                <Route path='/about' exact component={About}></Route>
+                <Route path='/contact' exact component={Contact}></Route>
             </div>
         )
     };
