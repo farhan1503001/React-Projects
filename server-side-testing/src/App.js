0a390ffbc21d31f2ae951d809901d80b55984533
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-12-19 13:19:48 +0600

    testing completed

--- server-side-testing/src/App.js
+++ server-side-testing/src/App.js
@@ -4,23 +4,41 @@ import React, { Component } from 'react';
 import axios from 'axios'
 class App extends Component{
   //react addition
+  //using state
+  state={
+    dishes:null,
+    errMessage:null
+  }
   componentDidMount(){
+    console.log("Initially: ",this.state)
     axios.get("http://localhost:3001/dishes")
     .then(response=>console.log(response.data))
-    .then(data=>console.log(data))
-    .catch(error=>console.log(error.message))
-
+    .then(data=>this.setState({
+      dishes:data
+    }))
+    .catch(error=>this.setState({
+      errMessage:error.message
+    }))
+    /*
     //now posting a value
     
-    //axios.post("http://localhost:3001/dishes",{name:"Italian Pasta",price:"450"})
-    //.then(response=>console.log(response))
+    axios.post("http://localhost:3001/dishes",{name:"Italian Pasta",price:"450"})
+    .then(response=>console.log(response))
     //now changing a value
-    //axios.put("http://localhost:3001/dishes/2",{name:"Chicken Burger",price:135})
-    //.then(response=>console.log(response))
-
+    axios.put("http://localhost:3001/dishes/2",{name:"Chicken Burger",price:135})
+    .then(response=>console.log(response))
+    */
     axios.delete("http://localhost:3001/dishes/2")
     .then(response=>console.log(response))
-    
+    .catch(error=>this.setState(
+      {
+        errMessage:error.message
+      }
+    ))
+  
+  }
+  componentDidUpdate(){
+    console.log("Updated: ",this.state);
   }
     render(){
       return(
